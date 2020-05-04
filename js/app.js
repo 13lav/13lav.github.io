bb50d1a11f3ea8f32d03efc8819ea3bea4ca4601
Lav Saini <lavsaini.mca19.du@gmail.com>
2020-05-04 22:34:03 +0530

    version 1.0

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const btnFetchLocation = document.getElementById('btn-fetch-location').addEventListener('click', startTrailing);
+
+function startTrailing() {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition((position) => {
+            const coords = [position.coords.latitude, position.coords.longitude];
+            const div = document.createElement('div');
+            div.className = 'alert alert-light';
+            div.innerHTML = `Latitude: ${position.coords.latitude} <strong> | </strong> Longitude: ${position.coords.longitude} <br/><i>${Date(position.timestamp)}</i>`;
+            document.querySelector('.trail-container').insertBefore(div, document.querySelector('.alert'));
+            
+        }, (error) => {
+            alert('Error while fetching your location. Try again.');
+        }, {
+            enableHighAccuracy: true,
+            maximumAge: 800
+        });
+    } else {
+        alert('Cannot access your location, please verify that your browser supports this feature.');
+    }
+}
